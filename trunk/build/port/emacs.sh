f108b863e970ac3e7c176460f283f32101dd5017
iforaker <iforaker@7cd04dbd-acc5-db11-a585-0003470a40ca>
2009-02-04 19:14:18 +0000

    Fixed emacs.sh for creating emacs link

--- trunk/build/port/emacs.sh
+++ trunk/build/port/emacs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 port install emacs +atsui +carbon
--e /opt/local/bin/emacs || ln -s /Applications/MacPorts/Emacs.app/Contents/MacOS/Emacs /opt/local/bin/emacs
+test -e /opt/local/bin/emacs || ln -s /Applications/MacPorts/Emacs.app/Contents/MacOS/Emacs /opt/local/bin/emacs
