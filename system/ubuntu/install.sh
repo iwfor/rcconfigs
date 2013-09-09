9ef8c0f332421d65a12e69b886f2bd314f28f546
Isaac Foraker <isaac@nosci.net>
2013-09-09 03:54:09 +0000

    Added ubuntu package setup

---
+++ system/ubuntu/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y \
+    autoconf \
+    automake \
+    build-essential \
+    clang \
+    git \
+    gnupg \
+    lftp \
+    libssl-dev \
+    libtool \
+    libyaml-dev \
+    p7zip-full \
+    screen \
+    subversion \
+    tmux \
+    vim \
+    whois \
+    zlib1g-dev \
+    zsh \
+    ;
