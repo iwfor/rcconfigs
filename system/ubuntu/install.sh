070e71e3b68e09e1db580c29a293a9b95a0fc1f5
Isaac Foraker <isaac@nosci.net>
2015-12-25 14:27:58 -0700

    Update Ubuntu system setup script

--- system/ubuntu/install.sh
+++ system/ubuntu/install.sh
@@ -9,22 +9,41 @@ sudo apt-get install -y \
     automake \
     build-essential \
     clang \
+    curl \
+    fbreader \
     git \
     gnupg \
     lftp \
+    libcurl4-openssl-dev \
+    libdvdread4 \
+    libexpat-dev \
     libreadline-dev \
     libsqlite3-dev \
     libssl-dev \
     libtool \
     libyaml-dev \
+    mpg123 \
+    mplayer \
     nodejs \
     p7zip-full \
+    python-pip \
+    python-virtualenv \
     screen \
     sqlite3 \
     subversion \
     tmux \
     vim \
+    vim-gtk \
+    vlc \
+    w3m-img \
     whois \
     zlib1g-dev \
     zsh \
     ;
+
+# If in a country that allows it, install deCSS for DVD play back:
+#sudo /usr/share/doc/libdvdread4/install-css.sh
+
+sudo add-apt-repository -y ppa:stebbins/handbrake-releases
+sudo apt-get update
+sudo apt-get install -y handbrake-gtk handbrake-cli
