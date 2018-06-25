6dec2a91f97b931c92907f9a3dbc2849235f008c
Isaac Foraker <isaac@nosci.net>
2018-06-24 19:35:10 -0600

    Update install-docker.sh to disable blocking systemctl call

--- system/ubuntu/install-docker.sh
+++ system/ubuntu/install-docker.sh
@@ -5,9 +5,10 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update
 sudo apt-get install -y docker-ce docker-compose
 sudo usermod -aG docker ${USER}
-sudo systemctl status docker
 
 declare DOCKERCFG=/etc/grub.d/42_docker
 echo '# Enable Linux kernel swap memory limit for docker' | sudo tee $DOCKERCFG
 echo 'GRUB_CMDLINE="cgroup_enable=memory swapaccount=1"' | sudo tee -a $DOCKERCFG
 sudo update-grub
+
+#sudo systemctl status docker
